6a8bca77c965fab3271bfdda37d1ba4a7328154d
Eli Barr <niteshadow53@gmail.com>
2018-12-31 16:55:30 -0500

    created trend micro spider

---
+++ news_scraper/news_scraper/spiders/TrendMicro_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+import json
+
+class TrendMicro_spider(scrapy.Spider):
+    name = "TrendMicro"
+
+    def start_requests(self):
+        urls = [
+            "https://blog.trendmicro.com/trendlabs-security-intelligence/"
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    # Get webpage
+    # Parse out article data
+    # Store and dedupe (not finished yet)
+    def parse(self, response):
+        articles = 
