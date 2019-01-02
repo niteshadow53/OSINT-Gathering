5fc7250c09efa3103dd910105c3a1e26b6727b1b
Eli Barr <niteshadow53@gmail.com>
2019-01-01 21:24:31 -0500

    trendmicro spider created and implemented

--- news_scraper/news_scraper/spiders/TrendMicro_spider.py
+++ news_scraper/news_scraper/spiders/TrendMicro_spider.py
@@ -1,7 +1,8 @@
 import scrapy
 import json
+import datetime
 
-class TrendMicro_spider(scrapy.Spider):
+class WeLiveSecurity_spider(scrapy.Spider):
     name = "TrendMicro"
 
     def start_requests(self):
@@ -15,4 +16,37 @@ class TrendMicro_spider(scrapy.Spider):
     # Parse out article data
     # Store and dedupe (not finished yet)
     def parse(self, response):
-        articles = ""
+        articles = response.css('#pageContent div.post')
+        # for a in articles:
+        #     self.log(a.extract())
+
+        article_title = ""
+        date_published = ""
+        short_description = ""
+        article_link = ""
+        author = ""
+
+        articles_parsed = []
+        for a in articles:
+            article_title = a.css("div.post-title h1::text").extract_first()
+            date_published = a.css("li.post-date a::text").extract_first() + " " + a.css("li.post-date span::text").extract_first()
+            short_description = a.css("div.post-text p::text").extract_first()
+            article_link = a.css("div.post-title a::attr(href)").extract_first()
+            author = ""
+
+            date_published = datetime.datetime.strptime(date_published, "%B %d, %Y at %H:%M %p").isoformat()
+            a_dict = {
+                "article_title": article_title.strip(),
+                "date_published": date_published,
+                "short_description": short_description,
+                "article_link": article_link,
+                "author": author,
+            }
+
+            print(a_dict)
+            articles_parsed.append(a_dict)
+            print("================")
+            # print(elt)
+
+        with open('trendmicro.json', 'w') as f:
+            f.write(json.dumps(articles_parsed))
